11bd07863d1cfdc3a1f077209d7e37121b8bac4f
Jhonel <jonichi16@gmail.com>
2023-11-08 09:27:44 +0800

    Add sliding animation to todo when deleting

--- src/TodoApp/components/Card.tsx
+++ src/TodoApp/components/Card.tsx
@@ -1,11 +1,9 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {Animated, Dimensions, StyleSheet, Text, View} from 'react-native';
+import React, {useRef} from 'react';
 import {Todo} from '../models/Todo';
 import CheckBox from './CheckBox';
 import {Spacing, Typography} from '../../common/styles';
 import {useTheme} from '@react-navigation/native';
-import Slide from '../../common/components/animations/Slide';
-import useSlide from '../../common/hooks/useSlide';
 
 type CardProps = {
   todo: Todo;
@@ -14,20 +12,23 @@ type CardProps = {
 
 const Card = ({todo, updateStatus}: CardProps) => {
   const {colors} = useTheme();
-  const {slideToRight} = useSlide();
+  const slide = useRef(new Animated.Value(0)).current;
 
   const onPress = () => {
-    slideToRight().start(() => updateStatus(todo.id));
-    // updateStatus(todo.id);
+    Animated.timing(slide, {
+      toValue: Dimensions.get('window').width,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start(() => updateStatus(todo.id));
   };
 
   return (
-    <Slide>
+    <Animated.View style={{transform: [{translateX: slide}]}}>
       <View style={[{backgroundColor: colors.primary}, styles.container]}>
         <CheckBox size={24} onPress={onPress} />
         <Text style={[{color: colors.text}, styles.task]}>{todo.task}</Text>
       </View>
-    </Slide>
+    </Animated.View>
   );
 };
 
