43c8e130c6f03d509ab9cf7a9b28e8f1e5488e4e
Jhonel <jonichi16@gmail.com>
2023-11-08 14:30:45 +0800

    Add color to status bar

--- App.tsx
+++ App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import AppNavigator from './src/navigations/AppNavigator';
+import {StatusBar} from 'react-native';
+import {Colors} from './src/common/styles';
 
 const App = () => {
-  return <AppNavigator />;
+  return (
+    <>
+      <StatusBar backgroundColor={Colors.primary.p900} />
+      <AppNavigator />
+    </>
+  );
 };
 
 export default App;
